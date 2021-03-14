31e08d2415641f47d5bb1eb9dbf2eef2a3b4fb22
borodulin_da <borodulin_da@srvhub.ru>
2021-03-14 13:39:00 +0300

    dev commit

--- admin/tables/email_servers.go
+++ admin/tables/email_servers.go
@@ -4,6 +4,7 @@ import (
 	"github.com/GoAdminGroup/go-admin/context"
 	"github.com/GoAdminGroup/go-admin/modules/db"
 	"github.com/GoAdminGroup/go-admin/plugins/admin/modules/table"
+	"github.com/GoAdminGroup/go-admin/template/types"
 	"github.com/GoAdminGroup/go-admin/template/types/form"
 )
 
@@ -25,25 +26,26 @@ func GetEmailServersTable(ctx *context.Context) (emailServersTable table.Table) 
 
 	info := emailServersTable.GetInfo()
 	info.AddField("ID", "id", db.Int).FieldSortable()
-	info.AddField("EmailDefaultFrom", "email_default_from", db.Varchar)
-	info.AddField("EmailHost", "email_host", db.Varchar)
-	info.AddField("EmailPort", "email_port", db.Int)
-	info.AddField("EmailUsername", "email_username", db.Varchar)
-	info.AddField("EmailUseSSL", "email_use_ssl", db.Boolean)
-	info.AddField("EmailUseTLS", "email_use_tls", db.Boolean)
-	info.AddField("EmailFailSilently", "email_fail_silently", db.Varchar)
-	info.AddField("EmailTimeout", "email_timeout", db.Int)
-	info.AddField("EmailSSLCertFile", "email_ssl_cert_file", db.Varchar)
-	info.AddField("EmailSSLKeyfile", "email_ssl_key_file", db.Varchar)
-	info.AddField("ApiKey", "api_key", db.Varchar)
-	info.AddField("ApiUsername", "api_username", db.Varchar)
-	info.AddField("ApiFromEmail", "api_from_email", db.Varchar)
-	info.AddField("ApiFromName", "api_from_name", db.Varchar)
+	//info.AddField("EmailDefaultFrom", "email_default_from", db.Varchar)
+	info.AddField("Хост", "email_host", db.Varchar)
+	info.AddField("Порт", "email_port", db.Int)
+	//info.AddField("EmailUsername", "email_username", db.Varchar)
+	info.AddField("SSL", "email_use_ssl", db.Boolean)
+	info.AddField("TLS", "email_use_tls", db.Boolean)
+	//info.AddField("EmailFailSilently", "email_fail_silently", db.Varchar)
+	//info.AddField("EmailTimeout", "email_timeout", db.Int)
+	//info.AddField("EmailSSLCertFile", "email_ssl_cert_file", db.Varchar)
+	//info.AddField("EmailSSLKeyfile", "email_ssl_key_file", db.Varchar)
+	//info.AddField("ApiKey", "api_key", db.Varchar)
+	//info.AddField("ApiUsername", "api_username", db.Varchar)
+	//info.AddField("ApiFromEmail", "api_from_email", db.Varchar)
+	//info.AddField("ApiFromName", "api_from_name", db.Varchar)
 	info.AddField("SendingMethod", "sending_method", db.Varchar)
-	info.AddField("IsActive", "is_active", db.Boolean)
-	info.AddField("PreferredDomains", "preferred_domains", db.Int)
+	info.AddField("Основной сервер", "main_server", db.Boolean)
+	info.AddField("Сервер активен", "is_active", db.Boolean)
+	//info.AddField("PreferredDomains", "preferred_domains", db.Int)
 
-	info.SetTable("email_servers").SetTitle("EmailServers").SetDescription("EmailServers")
+	info.SetTable("email_servers").SetTitle("Почтовые сервера").SetDescription("Почтовые сервера")
 
 	formList := emailServersTable.GetForm()
 	formList.AddField("ID", "id", db.Int, form.Default).FieldDisplayButCanNotEditWhenUpdate().FieldDisableWhenCreate()
@@ -51,10 +53,18 @@ func GetEmailServersTable(ctx *context.Context) (emailServersTable table.Table) 
 	formList.AddField("EmailHost", "email_host", db.Varchar, form.Text)
 	formList.AddField("EmailPort", "email_port", db.Int, form.Text)
 	formList.AddField("EmailUsername", "email_username", db.Varchar, form.Text)
-	formList.AddField("EmailUseSSL", "email_use_ssl", db.Boolean, form.Checkbox)
-	formList.AddField("EmailUseTLS", "email_use_tls", db.Boolean, form.Checkbox)
+	formList.AddField("SSL", "email_use_ssl", db.Boolean, form.Switch).
+		FieldOptions(types.FieldOptions{
+			{Value: "0"},
+			{Value: "1"},
+		})
+	formList.AddField("TLS", "email_use_tls", db.Boolean, form.Switch).
+		FieldOptions(types.FieldOptions{
+			{Value: "0"},
+			{Value: "1"},
+		})
 	formList.AddField("EmailFailSilently", "email_fail_silently", db.Varchar, form.Text)
-	formList.AddField("EmailTimeout", "email_timeout", db.Int, form.Number)
+	formList.AddField("EmailTimeout", "email_timeout", db.Int, form.Number).FieldDefault("0")
 	formList.AddField("EmailSSLCertFile", "email_ssl_cert_file", db.Varchar, form.File)
 	formList.AddField("EmailSSLKeyfile", "email_ssl_key_file", db.Varchar, form.File)
 	formList.AddField("ApiKey", "api_key", db.Varchar, form.Text)
@@ -62,10 +72,19 @@ func GetEmailServersTable(ctx *context.Context) (emailServersTable table.Table) 
 	formList.AddField("ApiFromEmail", "api_from_email", db.Varchar, form.Text)
 	formList.AddField("ApiFromName", "api_from_name", db.Varchar, form.Text)
 	formList.AddField("SendingMethod", "sending_method", db.Varchar, form.Text)
-	formList.AddField("IsActive", "is_active", db.Boolean, form.Checkbox)
-	formList.AddField("PreferredDomains", "preferred_domains", db.Int, form.Select)
+	formList.AddField("Основной сервер", "main_server", db.Boolean, form.Switch).
+		FieldOptions(types.FieldOptions{
+			{Value: "0"},
+			{Value: "1"},
+		})
+	formList.AddField("Сервер активен", "is_active", db.Boolean, form.Switch).
+		FieldOptions(types.FieldOptions{
+			{Value: "0"},
+			{Value: "1"},
+		})
+	formList.AddField("PreferredDomains", "preferred_domains", db.Tinyint, form.SelectSingle)
 
-	formList.SetTable("email_servers").SetTitle("EmailServers").SetDescription("EmailServers")
+	formList.SetTable("email_servers").SetTitle("Почтовый сервер").SetDescription("Почтовый сервер")
 
 	return
 }
