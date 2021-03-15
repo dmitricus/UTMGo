89578f7fdac0718dcf67d931ec77319695aa06bc
borodulin <borodulin_da@srvhub.ru>
2021-03-15 10:39:39 +0300

    dev commit

--- admin/models/base.go
+++ admin/models/base.go
@@ -7,17 +7,16 @@ import (
 )
 
 var (
-	orm *gorm.DB
+	ORM *gorm.DB
 	err error
 )
 
 func Init(c db.Connection) {
-	orm, err = gorm.Open("postgres", c.GetDB("default"))
-
+	ORM, err = gorm.Open("postgres", c.GetDB("default"))
 	if err != nil {
 		panic("initialize orm failed")
 	}
-	orm.AutoMigrate(
+	ORM.AutoMigrate(
 		&models.EmailServers{},
 		&models.Domains{},
 		&models.Bounced{},
--- api/controllers/version.go
+++ api/controllers/version.go
@@ -2,18 +2,20 @@ package controllers
 
 import (
 	"github.com/gin-gonic/gin"
-	//"net/http"
-	//"time"
+	"net/http"
+	"time"
+
+	"main/admin/models"
 )
 
 func GetVersion(context *gin.Context) {
-	//var now time.Time
-	//models.DB.Raw("SELECT NOW()").Scan(&now)
-	//context.JSON(http.StatusOK, gin.H{
-	//	"version":         "",
-	//	"build_time":      "",
-	//	"commit":          "",
-	//	"db_datetime":     now,
-	//	"server_datetime": time.Now(),
-	//})
+	var now time.Time
+	models.ORM.Raw("SELECT NOW()").Scan(&now)
+	context.JSON(http.StatusOK, gin.H{
+		"version":         "",
+		"build_time":      "",
+		"commit":          "",
+		"db_datetime":     now,
+		"server_datetime": time.Now(),
+	})
 }
